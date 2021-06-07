69b3ea39a879bff5a707710e7171d821298bff1a
wangechimk <wangechik.michelle@gmail.com>
2021-06-07 11:01:33 +0300

    add path routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent}  from './home/home.component';
+import {PostsComponent} from './posts/posts.component';
+import {ProfileComponent} from './profile/profile.component';
+import {BusinessComponent} from './business/business.component';
+import {NeighbourhoodComponent} from './neighbourhood/neighbourhood.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"posts", component:PostsComponent},
+  {path:"profile", component:ProfileComponent},
+  {path:"neighbourhood", component:NeighbourhoodComponent},
+  {path:"business", component:BusinessComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NeighbourhoodComponent } from './neighbourhood/neighbourhood.component';
+import { BusinessComponent } from './business/business.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    PostsComponent,
+    ProfileComponent,
+    NeighbourhoodComponent,
+    BusinessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
