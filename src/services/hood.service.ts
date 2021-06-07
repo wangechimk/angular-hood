da3a1856f89d60e5f3d0a7e0ab558d55d12eed9a
wangechimk <wangechik.michelle@gmail.com>
2021-06-07 21:23:35 +0300

    add the hood api

--- src/services/hood.service.ts
+++ src/services/hood.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HoodService {
+export class GethoodsService {
+  api_link:string='http://localhost:8000/';
 
-  constructor() { }
+  url = 'http://127.0.0.1:8000/api/hood/'
+  update_url ='http://127.0.0.1:8000/api/update/hood/'
+  
+//   constructor(private http:HttpClient) {
+
+//    }
+
+//    fetchHoodApi(): Observable<Hood[]>{
+//     return this.http.get<Hood[]>(this.url);
+//   }
+//   create(hood: any) {
+//     return this.http.post(this.url, hood);
+//   }
+
+//   update(id: any, hood: Hood) {
+//     return this.http.put(`${this.update_url}${id}/`, hood);
+//   }
+
+
 }
